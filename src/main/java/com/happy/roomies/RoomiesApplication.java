5f4f21f51890b322bc528bb6ef15b82bc3550a24
KOYN <gkeo933@gmail.com>
2026-02-21 17:51:01 +0200

    chore: update main for JPAAuditing and Async functions

--- src/main/java/com/happy/roomies/RoomiesApplication.java
+++ src/main/java/com/happy/roomies/RoomiesApplication.java
@@ -2,8 +2,12 @@ package com.happy.roomies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
 public class RoomiesApplication {
 
 	public static void main(String[] args) {
