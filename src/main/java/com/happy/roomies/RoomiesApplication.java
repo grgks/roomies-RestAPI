3fa31b2edd23d5a861c6a3dc39fbad709731186c
KOYN <gkeo933@gmail.com>
2026-02-21 17:40:57 +0200

    chore: initial project setup

---
+++ src/main/java/com/happy/roomies/RoomiesApplication.java
@@ -0,0 +1,13 @@
+package com.happy.roomies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/happy/roomies/RoomiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happy.roomies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
