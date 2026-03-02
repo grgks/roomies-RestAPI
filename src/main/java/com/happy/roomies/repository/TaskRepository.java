65e7783b4f7a6a9abbefdaf4a9f93c3d82f9e7ca
KOYN <gkeo933@gmail.com>
2026-03-02 23:12:48 +0200

    feat(repository): add derived query methods to TaskRepository interface

--- src/main/java/com/happy/roomies/repository/TaskRepository.java
+++ src/main/java/com/happy/roomies/repository/TaskRepository.java
@@ -1,12 +1,25 @@
 package com.happy.roomies.repository;
 
+import com.happy.roomies.core.enums.TaskStatus;
+import com.happy.roomies.model.House;
+import com.happy.roomies.model.Roommate;
 import com.happy.roomies.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<Task,Long>, JpaSpecificationExecutor<Task> {
 
 
+List<Task> findByTaskName(String taskName);
+List<Task> findByAssignees(Roommate assignees);
 
+List<Task> findByTaskStatus(TaskStatus taskStatus);
+
+List<Task> findByHouse(House house);
+List<Task> findByDueDateBetween(LocalDateTime startDate, LocalDateTime endDate);
 
 }
