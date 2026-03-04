8b7e4e442e06413cecefccaa285d74c208a9370d
KOYN <gkeo933@gmail.com>
2026-03-05 00:07:34 +0200

    chore(Test): update RoomiesApplicationTests to run test without contextLoads().

--- src/test/java/com/happy/roomies/RoomiesApplicationTests.java
+++ src/test/java/com/happy/roomies/RoomiesApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RoomiesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
