6fbdb68960336953fde29ecd772f88ab64a8af8d
KOYN <gkeo933@gmail.com>
2026-03-05 18:57:51 +0200

    feat(test): add TestDataFactory for less boilerplate code with:
    - createDefaultUser()
    - createDefaultHouse()
    - createDefaultRoommate()

---
+++ src/test/java/com/happy/roomies/util/TestDataFactory.java
@@ -0,0 +1,68 @@
+package com.happy.roomies.util;
+
+
+import com.happy.roomies.core.enums.Gender;
+import com.happy.roomies.model.House;
+import com.happy.roomies.model.Roommate;
+import com.happy.roomies.model.User;
+
+import java.util.List;
+
+/**
+ * Factory class for creating test data objects with default values.
+ * Reduces boilerplate code in tests.
+ */
+public class TestDataFactory {
+
+    //Entities
+
+    /**
+     * Creates a default User
+     */
+    public static User createDefaultUser(){
+        User user = new User();
+        user.setId(13L);
+        user.setKeycloakId("121-234-345-23a");
+        user.setEmail("user@aueb.gr");
+        user.setPhoneNumber("0987654321");
+        user.setIsActive(true);
+        return user;
+    }
+
+
+    /**
+     * Creates a default User
+     */
+    public static House createDefaultHouse(){
+        User user = TestDataFactory.createDefaultUser();
+
+        House house = new House();
+        house.setId(12L);
+        house.setAddress("Unknown Street");
+        house.setAddressNumber("12");
+        house.setApartment("21B");
+        house.setNumOfRooms(4L);
+        house.setOwner(user);
+        return house;
+    }
+
+    /**
+     * Creates a default Roommate with a default User
+     */
+
+    public static Roommate createDefaultRoommate() {
+        User user = TestDataFactory.createDefaultUser();
+        House house = TestDataFactory.createDefaultHouse();
+
+        Roommate roommate = new Roommate();
+        roommate.setId(12L);
+        roommate.setFirstname("Roommate1");
+        roommate.setLastname("RoommateLast");
+        roommate.setGender(Gender.FEMALE);
+        roommate.setUser(user);
+        roommate.setHouses(List.of(house));
+        return roommate;
+    }
+
+
+}
